bd049eb0a9629828d7044e58776ee2526ec234c0
deepakpathania <deepakpathania789@gmail.com>
2019-10-19 22:02:02 +0530

    :sparkles: Added sessions storage via redis

--- config/session.js
+++ config/session.js
@@ -35,5 +35,9 @@ module.exports.session = {
   // isSessionDisabled: function (req){
   //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
   // },
+  adapter: 'connect-redis',
+  host: 'localhost',
+  port: 6379,
+  db: 0,
 
 };
